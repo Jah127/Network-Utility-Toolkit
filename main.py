0d53895cedf6be6befb50a65e626aeebfc625a45
Jah127 <89108287+Jah127@users.noreply.github.com>
2026-07-10 21:02:49 +0000

    Add menu navigation and user input

--- main.py
+++ main.py
@@ -11,4 +11,15 @@ print("1. Ping Host")
 print("2. DNS Lookup")
 print("3. Port Scanner")
 print("4. Exit")
+print()
 
+where = float(input())
+
+if where == 1:
+    print("You selected Ping Host :)")
+elif where == 2:
+    print("You selected DNS Lookup :)")
+elif where == 3:
+    print("You selected Port Scanner :)")
+else:
+    print("You have exited the program :)")
