a0144356281298c7631fb9ef784d9ea4fc02f5d4
Jah127 <89108287+Jah127@users.noreply.github.com>
2026-07-13 21:10:07 +0000

    Added import from ping_tool.py into main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #Main entry point for Network Utility Toolkit
 #Displays the menu and routes the user to their desired tool
 
+from ping_tool import ping_host
+
 def display_menu():
     print("===========================")
     print("  Network Utility Toolkit")
@@ -12,10 +14,6 @@ def display_menu():
     print("2. DNS Lookup")
     print("3. Port Scanner")
     print("4. Exit")
-    print()
-
-def ping_host():
-    print("You selected Ping Host :)")
     print()
 
 def dns_lookup():
@@ -35,16 +33,16 @@ def exit_program():
 while True:
     display_menu()
     try:
-        option = int(input("Enter your choice here!!: "))
+        menu_choice = int(input("Enter your choice here!!: "))
         print()
 
-        if option == 1:
+        if menu_choice == 1:
             ping_host()
-        elif option == 2:
+        elif menu_choice == 2:
             dns_lookup()
-        elif option == 3:
+        elif menu_choice == 3:
             port_scanner()
-        elif option == 4:
+        elif menu_choice == 4:
             exit_program()
             break
         else:
--- ping_tool.py
+++ ping_tool.py
@@ -1,1 +1,6 @@
+#Contains the functionality for the Ping Host Tool.
+
+def ping_host():
+    address = input("Enter an IP address or hostname: ")
+    print(f"Pinging {address}...")
 
