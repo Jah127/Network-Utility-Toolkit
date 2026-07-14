d06812d8e5df9eb94bcece14b981a042258e0e2f
Jah127 <89108287+Jah127@users.noreply.github.com>
2026-07-14 21:17:48 +0000

    Still editing ping tool; will come back to

--- ping_tool.py
+++ ping_tool.py
@@ -1,6 +1,13 @@
 #Contains the functionality for the Ping Host Tool.
+import subprocess
 
 def ping_host():
     address = input("Enter an IP address or hostname: ")
+    print()
     print(f"Pinging {address}...")
 
+    command = ['ping', '-c', '1', address]
+    subprocess.run(command)
+
+    #print(subprocess)
+#ping_host()
